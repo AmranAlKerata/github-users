17cfcd1126beff481bab7049367b472e431a675f
Amran AL Ketara <amranalkeata@gmail.com>
2022-11-06 13:57:22 +0300

    loginWithRedirect

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,14 +2,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 import styled from "styled-components";
 import loginImg from "../images/login-img.svg";
 const Login = () => {
-  const { loginWithPopup } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
 
   return (
     <Wrapper>
       <div className="container">
         <img src={loginImg} alt="github user" />
         <h1>github user</h1>
-        <button className="btn" onClick={() => loginWithPopup()}>
+        <button className="btn" onClick={() => loginWithRedirect()}>
           login
         </button>
       </div>
